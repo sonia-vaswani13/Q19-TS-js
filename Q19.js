1cfbb3a01fb9edce25c76e297ae75420f023c702
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 13:34:04 -0700

    Q19 TS/js node js of 45 assignment
    Q19 TS/js node js of 45 assignment

---
+++ Q19.js
@@ -0,0 +1,6 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//Define an array of places to visit
+var placesToVisit = ["Singapore", "Newyork", "Islamabad", "India"];
+//Print the number of dinner guests
+console.log("number of dinner guest: ".concat(placesToVisit.length));
---
+++ Q19.ts
@@ -0,0 +1,7 @@
+"use strict"
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+//Define an array of places to visit
+let placesToVisit =["Singapore","Newyork","Islamabad","India"];
+//Print the number of dinner guests
+console.log(`number of dinner guest: ${placesToVisit.length}`);
